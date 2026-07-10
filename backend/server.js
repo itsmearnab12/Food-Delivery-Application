ef0fa4ad7cf815f04c33b5f021ae1831b800cd61
Arnab <arnabchoudhury2005@gmail.com>
2026-07-10 22:44:56 +0530

    working on backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+//app config
+const app = express();
+const PORT = 4000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
