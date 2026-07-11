06c37528be1940cf2a7d02951dbac8159b33fb95
Arnab <arnabchoudhury2005@gmail.com>
2026-07-11 22:43:26 +0530

    Working on admin panel

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { addFood, listFood } from "../controllers/foodController.js";
+import {
+  addFood,
+  listFood,
+  removeFood,
+} from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
@@ -16,5 +20,6 @@ const upload = multer({ storage });
 
 foodRouter.post("/add", upload.single("image"), addFood);
 foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
