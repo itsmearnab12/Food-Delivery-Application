2ab7f48b34010cffbc1e22e00cb2e7352eede5e3
Arnab <arnabchoudhury2005@gmail.com>
2026-07-15 19:51:13 +0530

    Working on authentication

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -7,6 +7,11 @@ import validator from "validator";
 const loginUser = async (req, res) => {};
 
 //Controller function for register user
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
 const registerUser = async (req, res) => {
   const { name, password, email } = req.body;
   try {
@@ -17,7 +22,37 @@ const registerUser = async (req, res) => {
     }
 
     //validating email format & strong password
-  } catch (error) {}
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    //hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
 export { loginUser, registerUser };
