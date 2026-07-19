1994a62345eefd26f9657b992c61c42f473baee9
Arnab <arnabchoudhury2005@gmail.com>
2026-07-19 23:29:59 +0530

    Working on backend

--- fontend/src/context/StoreContext.jsx
+++ fontend/src/context/StoreContext.jsx
@@ -1,13 +1,14 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
-
+import axios from "axios";
 export const StoreContext = createContext(null)
+
 
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
     const url = "http://localhost:4000"
     const [token, setToken] = useState("")
+    const [food_list, setFoodList] = useState([])
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -32,6 +33,21 @@ const StoreContextProvider = (props) => {
         }
         return totalAmount;
     }
+
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + "/api/food/list");
+        setFoodList(response.data.data)
+    }
+
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+    }, [])
 
     const contextValue = {
         food_list,
